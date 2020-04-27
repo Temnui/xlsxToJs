f013bf838bc479c61f72bec58c15550299921961
temnui <temnui@gmail.com>
2020-04-27 18:18:36 +0300

    new version with additional function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win = null;
 
 function createWindow() {
 	if(win) return;
-	win = new electron.BrowserWindow({width:800, height:600});
+	win = new electron.BrowserWindow({width:1200, height:700});
 	win.loadURL("file://" + __dirname + "/index.html");
 	win.webContents.openDevTools();
 	win.on('closed', function() { win = null; });
